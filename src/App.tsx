390644ad6f139c45ab8182e4ce44cacf61d45e65
ryujh030820 <ryujh030820@gmail.com>
2024-11-03 23:32:04 +0900

    feat: 포트폴리오 창 추가(#1)

--- src/App.tsx
+++ src/App.tsx
@@ -5,17 +5,24 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Home from './pages/Home.tsx';
 import Login from './pages/Login.tsx';
 import Signup from './pages/Signup.tsx';
+import Portfolio from './pages/Portfolio.tsx';
+import { RecoilRoot } from 'recoil';
 
 function App() {
   return (
-    <BrowserRouter>
-      <NavBar />
-      <Routes>
-        <Route path='/' element={<Home />} />
-        <Route path='/login' element={<Login />} />
-        <Route path='/signup' element={<Signup />} />
-      </Routes>
-    </BrowserRouter>
+    <RecoilRoot>
+      <BrowserRouter>
+        <NavBar />
+        <div className="container">
+          <Routes>
+            <Route path='/' element={<Home />} />
+            <Route path='/login' element={<Login />} />
+            <Route path='/signup' element={<Signup />} />
+            <Route path='/portfolio' element={<Portfolio />} />
+          </Routes>
+        </div>
+      </BrowserRouter>
+    </RecoilRoot>
   );
 }
 
